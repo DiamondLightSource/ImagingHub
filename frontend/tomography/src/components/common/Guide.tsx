42d878f043ce98d21ee10c8449ec0363fea74b6a
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-04-10 15:59:43 +0100

    Update guide page in tomo GUI

--- frontend/tomography/src/components/common/Guide.tsx
+++ frontend/tomography/src/components/common/Guide.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import SettingsIcon from "@mui/icons-material/Settings";
-import { Card, Typography } from "@mui/material";
+import { Card, List, ListItem, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Guide: React.FC = () => {
@@ -17,24 +17,39 @@ const Guide: React.FC = () => {
           borderRadius: "4px",
         }}
       >
-        <Typography variant="h6">What is tomohub ?</Typography>
-        <Typography variant="body1" gutterBottom>
-          a GUI built to work with{" "}
+        <Typography variant="h6">
+          GUI for{" "}
           <a
             href="https://diamondlightsource.github.io/httomo/index.html"
             target="_blank"
           >
-            HTTOMO package
+            HTTomo
           </a>
-          <br />
-          Tomohub provides all avalialbe methdods in HTTOMO (Httomolib,
-          Httomolibgpu) and allows users to setup their pipeline for
-          reconstruction, create/modify YAML config files and run the pipeline
-          with HTTOMO as well (powered by workflows)
         </Typography>
-        <Typography variant="h6">How to start ?</Typography>
         <Typography variant="body1" gutterBottom>
-          Start by opening <Link to="/methods">methods section</Link> setting up
+          <List dense={true} sx={{ listStyleType: "disc", pl: 2 }}>
+            <ListItem disableGutters={true} sx={{ display: "list-item" }}>
+              <Link to="methods"> Configure pipelines</Link>
+            </ListItem>
+            <ListItem disableGutters={true} sx={{ display: "list-item" }}>
+              Upload existing or download newly configured YAML pipeline files
+            </ListItem>
+            <ListItem disableGutters={true} sx={{ display: "list-item" }}>
+              <Link to="workflow-cor">
+                Interactively find the center-of-rotation (CoR)
+              </Link>
+            </ListItem>
+            <ListItem disableGutters={true} sx={{ display: "list-item" }}>
+              <Link to="crop"> Crop projections</Link>
+            </ListItem>
+            <ListItem disableGutters={true} sx={{ display: "list-item" }}>
+              <Link to="workflow-run">Run pipelines with HTTomo</Link>
+            </ListItem>
+          </List>
+        </Typography>
+        <Typography variant="h6">How to start?</Typography>
+        <Typography variant="body1" gutterBottom>
+          Start by opening <Link to="methods">methods section</Link> setting up
           your loader and then select your required methods afterwards
         </Typography>
         <Typography variant="h6">Further information</Typography>
