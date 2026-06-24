aa2389802645e7964ab8b8699f7e0b8c1541997c
frederick <frederick.allars@diamond.ac.uk>
2026-06-24 13:09:41 +0100

    use new resource component in the epsic page

--- frontend/ePSIC/src/App.tsx
+++ frontend/ePSIC/src/App.tsx
@@ -1,9 +1,11 @@
 import { WorkflowForm } from "./components/workflows/WorkflowForm";
+import { ResourceForm } from "./components/workflows/ResourceForm";
 
 export const App: React.FC = () => {
   return (
     <>
       <WorkflowForm />
+      <ResourceForm />
     </>
   );
 };
