6f989bf43e6d8de528aa43bd8a0275002223ee91
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-07-20 11:18:01 +0100

    Update `sci-react-ui` dependency to v0.6.2-alpha.1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
-import { ThemeProvider, DiamondTheme } from "@diamondlightsource/sci-react-ui";
+import {
+  ThemeProvider,
+  DiamondDSTheme,
+} from "@diamondlightsource/sci-react-ui";
 import { RelayEnvironmentProvider } from "react-relay";
 import { getRelayEnvironment } from "./RelayEnvironment";
 
 getRelayEnvironment().then((environment) => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <ThemeProvider theme={DiamondTheme} defaultMode="light">
+      <ThemeProvider theme={DiamondDSTheme} defaultMode="light">
         <CssBaseline />
         <RelayEnvironmentProvider environment={environment}>
           <App />
