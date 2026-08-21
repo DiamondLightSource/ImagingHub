6fea2849b05c5e5844c6735ee40b0f2ce201264d
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-08-21 11:45:07 +0100

    Add missing beamline property on type

--- frontend/unified/src/types/workflowFields.ts
+++ frontend/unified/src/types/workflowFields.ts
@@ -2,5 +2,6 @@ export type WorkflowFormData = {
   visit: string;
   template: string;
   outpath: string;
+  Beamline: string;
 };
 export type Option = { label: string; value: string; desc?: string };
