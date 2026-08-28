8bb838fbc9724102e19cf5de9ff6b5a64a7970e3
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-08-28 15:46:22 +0100

    Add skeleton CoR sweep template param config component

--- frontend/unified/src/components/ParameterConfiguration/ParameterConfiguration.tsx
+++ frontend/unified/src/components/ParameterConfiguration/ParameterConfiguration.tsx
@@ -2,7 +2,8 @@ import { Button, Typography } from "@mui/material";
 import { Option, Technique } from "../../types";
 import OptionSelect from "../OptionSelect";
 import { CorSweepParameterConfiguration } from "./TomoParameterConfiguration";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { LoaderProvider } from "../../../../tomography/src/contexts/LoaderContext";
 
 type ParameterConfigurationProps = {
   technique: Technique;
@@ -28,12 +29,30 @@ export const ParameterConfiguration: React.FC<ParameterConfigurationProps> = ({
   setTemplate,
   availableTemplates,
 }: ParameterConfigurationProps) => {
+  const [templateParameters, setTemplateParameters] = useState<object>({});
+  const [resourceParameters] = useState({
+    nprocs: 1,
+    memory: "20Gi",
+  });
+
+  const handleSubmitJob = () => {
+    console.log("Submit job");
+    const parameters = { ...templateParameters, ...resourceParameters };
+    // TODO: Send mutation
+  };
+
   const TEMPLATE_TO_COMPONENT_MAPPING: TemplateComponentMapping = {
     [Technique.Dpc]: {},
     [Technique.Nbed]: {},
     [Technique.Ptycho]: {},
     [Technique.Tomo]: {
-      "httomo-cor-sweep": <CorSweepParameterConfiguration />,
+      "httomo-cor-sweep": (
+        <LoaderProvider>
+          <CorSweepParameterConfiguration
+            setParameters={setTemplateParameters}
+          />
+        </LoaderProvider>
+      ),
     },
     [Technique.Xanes]: {},
     [Technique.Xrd]: {},
@@ -58,11 +77,7 @@ export const ParameterConfiguration: React.FC<ParameterConfigurationProps> = ({
         PlaceholderComponent}
 
       <div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => console.log("Submit job")}
-        >
+        <Button variant="contained" color="primary" onClick={handleSubmitJob}>
           Submit job
         </Button>
       </div>
--- frontend/unified/src/components/ParameterConfiguration/TomoParameterConfiguration.tsx
+++ frontend/unified/src/components/ParameterConfiguration/TomoParameterConfiguration.tsx
@@ -1,22 +1,145 @@
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
-import { useState } from "react";
+import {
+  Card,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { TemplateComponentProps } from "../../types";
+import Loader from "../../../../tomography/src/components/loader/Loader";
+import { useLoader } from "../../../../tomography/src/contexts/LoaderContext";
+import ParameterSweepForm from "../../../../tomography/src/components/workflows/sweepPipeline/ParameterSweepForm";
 
-export const CorSweepParameterConfiguration = () => {
+const HTTOMO_OUTDIR_NAME = "sweep-run";
+
+export const CorSweepParameterConfiguration = ({
+  setParameters,
+}: TemplateComponentProps) => {
   const [applyNormalisation, setApplyNormalisation] = useState<boolean>(true);
+  const [sweepValues, setSweepValues] = useState({
+    start: 0,
+    stop: 10,
+    step: 1,
+  });
+  const [outputFolder, setOutputFolder] = useState<string>("");
+
+  const {
+    method: loaderMethod,
+    module_path: loaderModulePath,
+    parameters: loaderParams,
+  } = useLoader();
+
+  const generatePipeline = () => {
+    let pipeline = [
+      {
+        method: loaderMethod,
+        module_path: loaderModulePath,
+        parameters: loaderParams,
+      },
+      {
+        method: "recon",
+        module_path: "tomopy.recon.algorithm",
+        parameters: {
+          center: sweepValues,
+          sinogram_order: false,
+          algorithm: "gridrec",
+          init_recon: null,
+        },
+      },
+    ];
+
+    const normalisationMethods = [
+      {
+        method: "normalize",
+        module_path: "tomopy.prep.normalize",
+        parameters: { cutoff: null, averaging: "mean" },
+      },
+      {
+        method: "minus_log",
+        module_path: "tomopy.prep.normalize",
+        parameters: {},
+      },
+    ];
+
+    if (applyNormalisation) {
+      pipeline = [pipeline[0], ...normalisationMethods, pipeline[1]];
+    }
+
+    return pipeline;
+  };
+
+  useEffect(() => {
+    setParameters({
+      config: generatePipeline(),
+      "httomo-outdir-name": HTTOMO_OUTDIR_NAME,
+      output: outputFolder,
+    });
+  }, [applyNormalisation, sweepValues, outputFolder]);
 
   return (
     <>
-      <FormGroup>
-        <FormControlLabel
-          label="Apply normalisation to raw data"
-          control={
-            <Checkbox
-              checked={applyNormalisation}
-              onChange={(e) => setApplyNormalisation(e.target.checked)}
-            />
-          }
+      <Loader />
+      <Card
+        variant="outlined"
+        sx={{
+          mx: "auto",
+          mb: 2,
+          p: 2,
+          border: "1px solid #89987880",
+          borderRadius: "4px",
+        }}
+      >
+        <Typography
+          gutterBottom
+          variant="h6"
+          color="primary"
+          component="div"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <strong>Sweep configuration</strong>
+        </Typography>
+        <ParameterSweepForm values={sweepValues} onChange={setSweepValues} />
+      </Card>
+      <Card
+        variant="outlined"
+        sx={{
+          mx: "auto",
+          mb: 2,
+          p: 2,
+          border: "1px solid #89987880",
+          borderRadius: "4px",
+        }}
+      >
+        <Typography
+          gutterBottom
+          variant="h6"
+          color="primary"
+          component="div"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <strong>Pipeline configuration</strong>
+        </Typography>
+        <FormGroup>
+          <FormControlLabel
+            label="Apply normalisation to raw data"
+            control={
+              <Checkbox
+                checked={applyNormalisation}
+                onChange={(e) => setApplyNormalisation(e.target.checked)}
+              />
+            }
+          />
+        </FormGroup>
+        <TextField
+          variant="outlined"
+          size="small"
+          label="Output folder"
+          value={outputFolder}
+          onChange={(e) => setOutputFolder(e.target.value)}
         />
-      </FormGroup>
+      </Card>
     </>
   );
 };
