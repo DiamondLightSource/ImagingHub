af5760f79d8ce94d764d61748c4a67bb05fffa94
frederick <frederick.allars@diamond.ac.uk>
2026-09-18 15:48:27 +0100

    adding ptyrex configuration component

---
+++ frontend/unified/src/components/ParameterConfiguration/PtyrexParameterConfiguration.tsx
@@ -0,0 +1,60 @@
+import {
+  Card,
+  Stack,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { TemplateComponentProps } from "../../types";
+
+const HTTOMO_OUTDIR_NAME = "sweep-run";
+
+export const PtyrexParameterConfiguration = ({
+  setParameters,
+}: TemplateComponentProps) => {
+  const [configPath, setConfigPath] = useState<string>("");
+  const [projectionNumber, setProjectionNumber] = useState<string>("");
+
+  useEffect(() => {
+    setParameters({
+      config_json: configPath,
+      scan_number: 398894,
+      projection_number: projectionNumber,
+      nprocs: 4,
+      memory: "32Gi",
+    });
+  }, [configPath, projectionNumber]);
+
+  console.log(`configPath: ${configPath}`);
+  console.log(`projectionNumber: ${projectionNumber}`);
+
+  return (
+    <>
+      <Card
+        variant="outlined"
+        sx={{
+          mb: 2,
+          p: 2,
+          border: "1px solid #89987880",
+          borderRadius: "4px",
+        }}
+      >
+        <Stack direction="column" spacing={1}>
+          <TextField
+            label={"Ptyrex config file path"}
+            value={configPath}
+            onChange={(e) => setConfigPath(e.target.value)}
+          />
+          <TextField
+            label={"Projection Number"}
+            value={projectionNumber}
+            onChange={(e) => setProjectionNumber(e.target.value)}
+          />
+        </Stack>
+      </Card>
+    </>
+  );
+};
