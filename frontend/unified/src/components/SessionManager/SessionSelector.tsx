7fcb434d0adff55e73f7835cedfe22aef1263c26
NatLeung96 <nathanael.leung96@gmail.com>
2026-09-24 12:58:30 +0000

    add new SessionSelector

--- frontend/unified/src/components/SessionManager/SessionSelector.tsx
+++ frontend/unified/src/components/SessionManager/SessionSelector.tsx
@@ -1,21 +1,22 @@
-import React, { Suspense, useState } from "react";
-import {
-  TextField,
-  ToggleButton,
-  ToggleButtonGroup,
-  Stack,
-  Button,
-  Tooltip,
-} from "@mui/material";
-import { SessionQueryQuery } from "./__generated__/SessionManager.generated";
+import { gql, TypedDocumentNode } from "@apollo/client";
 import {
   GetSessionByReferenceQuery,
   GetSessionByReferenceQueryVariables,
 } from "./__generated__/SessionSelector.generated";
-import { visitRegex } from "@diamondlightsource/sci-react-ui";
-import { gql, TypedDocumentNode } from "@apollo/client";
-import { useSuspenseQuery } from "@apollo/client/react";
-import { SessionSelectionMode } from "../../types";
+import { SessionQueryQuery } from "./__generated__/SessionManager.generated";
+import React, { useEffect, useState } from "react";
+import { Beamline, SessionSelectionMode } from "../../types";
+import { Visit, visitRegex } from "@diamondlightsource/sci-react-ui";
+import {
+  Button,
+  OutlinedInput,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
+} from "@mui/material";
+import { useQuery } from "@apollo/client/react";
+import { InfoIcon } from "lucide-react";
 
 export const GET_SESSION_BY_REFERENCE: TypedDocumentNode<
   GetSessionByReferenceQuery,
@@ -41,118 +42,229 @@ type NonNullAccount = NonNullable<SessionQueryQuery["account"]>;
 export type InstrumentSession =
   NonNullAccount["instrumentSessionRoles"]["edges"][0]["node"]["instrumentSession"];
 
-type SessionSelectorProps = {
-  setSession: (_: InstrumentSession | null) => void;
-  mode: SessionSelectionMode;
-  setMode: (_: SessionSelectionMode) => void;
-};
+const SessionSelector = ({
+  latestSession,
+  beamline,
+  setBeamline,
+  visit,
+  setVisit,
+}: {
+  latestSession: InstrumentSession;
+  beamline: Beamline | null;
+  setBeamline: (_: Beamline | null) => void;
+  visit: Visit | null;
+  setVisit: (_: Visit | null) => void;
+}) => {
+  const [sessionSelectionMode, setSessionSelectionMode] =
+    useState<SessionSelectionMode>(SessionSelectionMode.Latest);
+  const [customSession, setCustomSession] =
+    useState<InstrumentSession>(latestSession);
 
-export const SessionSelector: React.FC<SessionSelectorProps> = ({
-  setSession,
-  mode,
-  setMode,
-}: SessionSelectorProps) => {
-  const [customeSessionInputValue, setCustomSessionInputValue] =
-    useState<string>("");
+  useEffect(() => {
+    if (!beamline) {
+      setBeamline(latestSession.instrument.name as Beamline);
+    }
+
+    if (!visit) {
+      setVisit({
+        proposalCode: latestSession.proposal.proposalCategory?.toLowerCase(),
+        proposalNumber: latestSession.proposal.proposalNumber,
+        number: latestSession.instrumentSessionNumber,
+      } as Visit);
+    }
+  }, [
+    beamline,
+    setBeamline,
+    visit,
+    setVisit,
+    latestSession.instrument.name,
+    latestSession.proposal.proposalNumber,
+    latestSession.proposal.proposalCategory,
+    latestSession.instrumentSessionNumber,
+  ]);
+
+  const handleChange = (
+    _: React.MouseEvent<HTMLElement>,
+    toggleButtonLabel: string
+  ) => {
+    if (toggleButtonLabel === SessionSelectionMode.Latest) {
+      setSessionSelectionMode(toggleButtonLabel);
+      setVisit({
+        proposalCode: latestSession.proposal.proposalCategory?.toLowerCase(),
+        proposalNumber: latestSession.proposal.proposalNumber,
+        number: latestSession.instrumentSessionNumber,
+      } as Visit);
+      setBeamline(latestSession.instrument.name as Beamline);
+    } else if (toggleButtonLabel === SessionSelectionMode.Custom) {
+      setSessionSelectionMode(toggleButtonLabel);
+      setVisit({
+        proposalCode: customSession.proposal.proposalCategory?.toLowerCase(),
+        proposalNumber: customSession.proposal.proposalNumber,
+        number: customSession.instrumentSessionNumber,
+      } as Visit);
+      setBeamline(customSession.instrument.name as Beamline);
+    }
+  };
 
   return (
-    <Stack direction="row" spacing={2} alignItems={"center"}>
+    <Stack direction="row" spacing={1}>
       <ToggleButtonGroup
         exclusive
-        value={mode}
-        onChange={(_, toggleButtonLabel: string) => {
-          if (toggleButtonLabel === SessionSelectionMode.Latest) {
-            setMode(SessionSelectionMode.Latest);
-          } else if (toggleButtonLabel === SessionSelectionMode.Custom) {
-            setMode(SessionSelectionMode.Custom);
-          }
-        }}
+        value={sessionSelectionMode}
+        onChange={handleChange}
       >
-        <ToggleButton
-          sx={{ textTransform: "none" }}
-          value={SessionSelectionMode.Latest}
-        >
+        <ToggleButton value={SessionSelectionMode.Latest}>
           {SessionSelectionMode.Latest}
         </ToggleButton>
-        <ToggleButton
-          sx={{ textTransform: "none" }}
-          value={SessionSelectionMode.Custom}
-        >
+        <ToggleButton value={SessionSelectionMode.Custom}>
           {SessionSelectionMode.Custom}
         </ToggleButton>
       </ToggleButtonGroup>
-      <TextField
-        variant="outlined"
-        label="Session"
-        disabled={mode === SessionSelectionMode.Latest}
-        value={customeSessionInputValue}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setCustomSessionInputValue(e.target.value);
-        }}
+      <CustomSessionInput
+        sessionSelectionMode={sessionSelectionMode}
+        setCustomSession={setCustomSession}
+        setVisit={setVisit}
+        setBeamline={setBeamline}
       />
-      <Suspense fallback={<p>Checking session...</p>}>
-        <SelectSessionButton
-          sessionInputValue={customeSessionInputValue}
-          sessionSelectionMode={mode}
-          setSession={setSession}
-        />
-      </Suspense>
     </Stack>
   );
 };
 
-type SelectSessionButtonProps = {
-  sessionInputValue: string;
-  sessionSelectionMode: SessionSelectionMode;
-  setSession: (_: InstrumentSession | null) => void;
-};
-
-const SelectSessionButton: React.FC<SelectSessionButtonProps> = ({
-  sessionInputValue,
+const CustomSessionInput = ({
   sessionSelectionMode,
-  setSession,
+  setCustomSession,
+  setVisit,
+  setBeamline,
+}: {
+  sessionSelectionMode: SessionSelectionMode;
+  setCustomSession: (_: InstrumentSession) => void;
+  setVisit: (_: Visit) => void;
+  setBeamline: (_: Beamline) => void;
 }) => {
-  const { data } = useSuspenseQuery(GET_SESSION_BY_REFERENCE, {
-    variables: { reference: sessionInputValue },
-  });
+  const [input, setInput] = useState<string>("");
+  const [pressed, setPressed] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isErrored, setIsErrored] = useState<boolean>(false);
+  const [helperText, setHelperText] = useState<string>(
+    "Session format: ab12345-1"
+  );
 
-  const isDisabled = () => {
-    if (sessionSelectionMode === SessionSelectionMode.Latest) {
-      return true;
+  const disabled = sessionSelectionMode !== SessionSelectionMode.Custom;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const text = event.target.value;
+    setInput(text);
+    if (visitRegex.exec(text) === null) {
+      setIsErrored(true);
+      setHelperText("Session format: ab12345-1");
     } else {
-      return (
-        visitRegex.exec(sessionInputValue) === null ||
-        data.instrumentSessionByReference === null
-      );
-    }
-  };
-
-  const generateTooltipText = () => {
-    if (sessionSelectionMode === SessionSelectionMode.Latest) {
-      return "";
-    } else if (visitRegex.exec(sessionInputValue) === null) {
-      return "Session must be of the following format: abcdef12345-1";
-    } else if (
-      visitRegex.exec(sessionInputValue) !== null &&
-      data.instrumentSessionByReference === null
-    ) {
-      return `The session ${sessionInputValue} doesn't exist`;
+      setIsErrored(false);
+      setHelperText("");
     }
   };
 
   return (
-    <Tooltip title={generateTooltipText()}>
-      <span>
-        <Button
-          data-testid="select-session-button"
-          variant="contained"
-          color="primary"
-          onClick={() => setSession(data.instrumentSessionByReference)}
-          disabled={isDisabled()}
-        >
-          Select session
-        </Button>
-      </span>
-    </Tooltip>
+    <>
+      <OutlinedInput
+        error={isErrored}
+        disabled={disabled}
+        onChange={handleChange}
+        endAdornment={
+          <Tooltip title={helperText}>
+            <InfoIcon />
+          </Tooltip>
+        }
+      />
+      <Button
+        disabled={disabled}
+        loading={loading}
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setPressed(true);
+        }}
+      >
+        Submit
+      </Button>
+      {pressed && (
+        <RunQuery
+          input={input}
+          setLoading={setLoading}
+          setVisit={setVisit}
+          setBeamline={setBeamline}
+          setCustomSession={setCustomSession}
+          setIsErrored={setIsErrored}
+          setHelperText={setHelperText}
+          setPressed={setPressed}
+        />
+      )}
+    </>
   );
 };
+
+const RunQuery = ({
+  input,
+  setLoading,
+  setVisit,
+  setBeamline,
+  setCustomSession,
+  setIsErrored,
+  setHelperText,
+  setPressed,
+}: {
+  input: string;
+  setLoading: (_: boolean) => void;
+  setVisit: (_: Visit) => void;
+  setBeamline: (_: Beamline) => void;
+  setCustomSession: (_: InstrumentSession) => void;
+  setIsErrored: (_: boolean) => void;
+  setHelperText: (_: string) => void;
+  setPressed: (_: boolean) => void;
+}) => {
+  const { data, error, loading } = useQuery(GET_SESSION_BY_REFERENCE, {
+    variables: { reference: input },
+  });
+
+  useEffect(() => {
+    if (loading) setLoading(true);
+    if (error) {
+      setIsErrored(true);
+      setHelperText(`Error: ${error.message}`);
+      setLoading(false);
+      setPressed(false);
+    }
+
+    if (data) {
+      const fetchedBeamline =
+        data.instrumentSessionByReference?.instrument.name;
+      if (data.instrumentSessionByReference === null) {
+        setIsErrored(true);
+        setHelperText(`Session ${input} does not exist`);
+        setLoading(false);
+        setPressed(false);
+      } else if (
+        !Object.values(Beamline).includes(fetchedBeamline as Beamline)
+      ) {
+        setIsErrored(true);
+        setHelperText(`${input} is not associated with an imaging beamline`);
+        setLoading(false);
+        setPressed(false);
+      } else {
+        setCustomSession(data.instrumentSessionByReference);
+        setVisit({
+          proposalCode:
+            data.instrumentSessionByReference.proposal.proposalCategory?.toLowerCase(),
+          proposalNumber:
+            data.instrumentSessionByReference.proposal.proposalNumber,
+          number: data.instrumentSessionByReference.instrumentSessionNumber,
+        } as Visit);
+        setBeamline(fetchedBeamline as Beamline);
+        setLoading(false);
+        setPressed(false);
+      }
+    }
+  });
+  return null;
+};
+
+export default SessionSelector;
