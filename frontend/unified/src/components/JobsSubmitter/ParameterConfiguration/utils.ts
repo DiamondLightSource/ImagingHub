f824e86c19af9f72ad702451c4f563f6a9862180
NatLeung96 <nathanael.leung96@gmail.com>
2026-09-24 14:53:09 +0000

    add raw data mappings to utils

--- frontend/unified/src/components/JobsSubmitter/ParameterConfiguration/utils.ts
+++ frontend/unified/src/components/JobsSubmitter/ParameterConfiguration/utils.ts
@@ -1,8 +1,17 @@
 import { templateOptions } from "../../../data/templates";
-import { Technique } from "../../../types";
+import { Beamline, Technique } from "../../../types";
 
 export const filterTemplates = (technique: Technique) => {
   return templateOptions.filter((option) =>
     option.value.includes(technique.toLowerCase())
   );
+};
+
+const DLS_FILESYSTEM_BEAMLINE_RAW_DATA_DIR_MAPPINGS = {
+  [Beamline.DIAD]: "nexus",
+  [Beamline["I08-1"]]: "nexus",
+  [Beamline.I12]: "rawdata",
+  [Beamline["I13-1"]]: "raw",
+  [Beamline["I13-2"]]: "raw",
+  [Beamline.I14]: "scan",
 };
